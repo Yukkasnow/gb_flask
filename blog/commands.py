696a0fc5f1c86859753f993928c6bb4ce9c2eb2a
YuliaE <yulia2000.egorova@yandex.ru>
2023-01-15 00:30:32 +0300

    done on my computer

--- blog/commands.py
+++ blog/commands.py
@@ -24,8 +24,17 @@ def create_init_user():
             User(email='name@example.com', password=generate_password_hash('test123'))
         )
         db.session.commit()
+
 @click.command('create-init-article')
 def create_init_user():
+    from blog.models import User
+    from wsgi import app
+
+    with app.app_context():
+        db.session.add(
+            User(email='name@example.com', password=generate_password_hash('test123'))
+        )
+        db.session.commit()
     from blog.models import Article
 
     from wsgi import app
--- blog/config.py
+++ blog/config.py
@@ -11,5 +11,5 @@ DEBUG = ENV == EnvType.development
 
 SECRET_KEY = 'SECRET_KEY'
 
-SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/test.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
